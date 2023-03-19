a810b25406381734e12ec2da9a96a11b6d4368a1
Lucas <lwedson12@gmail.com>
2023-03-19 20:33:05 -0300

    Refatorando o método update

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -42,13 +42,6 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
-  }
-
   async create({
     name, email, phone, category_id,
   }) {
@@ -62,25 +55,22 @@ class ContactsRepository {
     return row;
   }
 
-  update(id, {
-    name,
-    email,
-    phone,
-    category_id,
+  async update(id, {
+    name, email, phone, category_id,
   }) {
-    return new Promise((resolve) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id,
-      };
-      contacts = contacts.map((contact) =>{
-        contact.id === id ? updatedContact : contact;
-      });
+    const [row] = await db.query(`
+      UPDATE contacts
+      SET name = $1, email = $2, phone = $3, category_id = $4
+      WHERE id = $5
+      RETURNING *
+     `,[name, email, phone, category_id, id]);
+     return row;
+  }
 
-      resolve(updatedContact);
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 
